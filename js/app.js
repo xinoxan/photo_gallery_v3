18d81f1eefed1dc3feb66493794475af3dc65b5c
“xinoxan” <xinoxan@gmail.comssh-keygen -t rsa -C xinoxan@gmail.com>
2017-11-11 18:06:16 +0100

    responsive photo gallery using flexbox, jQuery lightcase plugin for the lightbox and a search filter

---
+++ js/app.js
@@ -0,0 +1,17 @@
+ $('#myInput').on('keyup change', function() {
+      var input, col, img;
+      input = document.getElementById('myInput');
+      input = input.value.toUpperCase();
+      col = document.getElementsByClassName('col');
+      
+// loop
+
+      for (var i = 0; i < col.length; i++) {
+        img = col[i].getElementsByTagName('img')[0];
+        if (img.alt.toUpperCase().indexOf(input) > -1) {
+          col[i].style.display = '';
+        } else {
+          col[i].style.display = 'none';
+        }
+      }
+    });

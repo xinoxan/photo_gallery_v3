7ea34364810b452fae757efcb86592b6a58c0373
“xinoxan” <xinoxan@gmail.comssh-keygen -t rsa -C xinoxan@gmail.com>
2017-11-13 10:54:13 +0100

    modify caption text

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
+ 
+// retrieve input search functionality
+
  $('#myInput').on('keyup change', function() {
       var input, col, img;
       input = document.getElementById('myInput');
       input = input.value.toUpperCase();
       col = document.getElementsByClassName('col');
       
-// loop
+// loop through all captions img alt
 
       for (var i = 0; i < col.length; i++) {
         img = col[i].getElementsByTagName('img')[0];
